3a56caa138c478e5cd966c78b8e6abda85270b9e
hharshithaj2000 <81506610+hharshithaj2000@users.noreply.github.com>
2022-03-15 12:25:40 +0530

    Update constructor.js

--- constructor.js
+++ constructor.js
@@ -11,20 +11,20 @@ class Student
         this.comp=comp;
     }
     sumofsub(){
-        return (this.math+this.sci+this.comp)/3;
+        return (this.math+this.sci+this.comp);
     }
 }
-let obj1=new Student("h","j","566","5",{math:56,sci:20,comp:10});
+let obj1=new Student("h","j","566","5",{math:75,sci:72,comp:85});
 console.log(obj1.sumofsub());
 
-let obj2=new Student("h","j","566","5",{math:20,sci:20,comp:10});
+let obj2=new Student("h","j","566","5",{math:98,sci:77,comp:87});
 console.log(obj2.sumofsub());
 
-let obj3=new Student("h","j","566","5",{math:57,sci:24,comp:14});
+let obj3=new Student("h","j","566","5",{math:78,sci:70,comp:68});
 console.log(obj3.sumofsub());
 
-let obj4=new Student("h","j","566","5",{math:78,sci:24,comp:76});
+let obj4=new Student("h","j","566","5",{math:79,sci:85,comp:90});
 console.log(obj4.sumofsub());
 
-let obj5=new Student("h","j","566","5",{math:56,sci:20,comp:13});
+let obj5=new Student("h","j","566","5",{math:65,sci:60,comp:72});
 console.log(obj5.sumofsub());
